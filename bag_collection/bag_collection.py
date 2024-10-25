733bbac4e2df5e4f85a5fc69810ea387d1280404
Field User <field@ccom.unh.edu>
2024-10-25 22:33:55 +0000

    Changed file list to info dictionary.

--- bag_collection/bag_collection.py
+++ bag_collection/bag_collection.py
@@ -3,34 +3,35 @@
 import os
 import rosbag
 import re
+import numpy as np
+import datetime
 
 class bag_collection():
     def __init__(self,directory):
 
         self.directory = directory
-        self.file_list = None
-        self.start_time = None
-        self.stop_time = None
+        self.bagfiles = []
         self.file_index = None
         self.topics = set()
         self.msg_types = set()
 
     def find_bag_files(self,path, extension='.bag'):
         """Recursively finds all files with the given extension in the specified directory."""
-        self.file_list = []
+        
         for root, dirs, files in os.walk(path):
             for file in files:
                 if file.endswith(extension):
-                    self.file_list.append(os.path.join(root, file))
+                    info = {'path': os.path.join(root, file)}
+                    self.bagfiles.append(info)
                     # yield os.path.join(root, file)
 
     def print_bag_files(self):
         '''Print a list of the bag files in the collection.'''
-        if self.file_index is None:
+        if self.bagfiles is None:
             self.find_bag_files(path.self.directory)
 
-        for f in self.file_list:
-            print("\t%s" % f)
+        for f in self.bagfiles:
+            print("\t%s" % f['path'])
 
 
     def index_collection(self):
@@ -44,29 +45,43 @@ class bag_collection():
         print("Indexing collection...")
         self.msg_types = set()
         self.topics = set()
-        if self.file_list is None:
+        if len(self.bagfiles) == 0:
             self.find_bag_files(path=self.directory)
         z = 0
-        for bagfile in self.file_list:
-            b = rosbag.Bag(bagfile)
-            tt = b.get_type_and_topic_info()
-            self.msg_types = self.msg_types.union(tt.msg_types.keys())
-            self.topics = self.topics.union(tt.topics.keys())
-            z = z + 1
-            if z % np.floor(len(self.file_list)/10) == 0:
-                print("%0.1f\% complete..." % z/len(self.file_list)*100)
+        toskip=[]
+        for baginfo in self.bagfiles:
+            dt = datetime.datetime.now()
+            b = rosbag.Bag(baginfo['path'])
+            try:
+                baginfo.update({'start_time': b.get_start_time()})
+                baginfo.update({'end_time': b.get_end_time()})
+            except rosbag.ROSBagException:
+                print("Error. Skipping %s" % baginfo["path"])
+                toskip.append(z)
+            print(baginfo)
+            z+=1
+            print("%d, %f" % (z,(datetime.datetime.now()-dt).total_seconds()) )
+        # remote files that don't records
+        for i in sorted(toskip, reverse=True):
+            del self.bagfiles[i]
+            #tt = b.get_type_and_topic_info()
+            #self.msg_types = self.msg_types.union(tt.msg_types.keys())
+            #self.topics = self.topics.union(tt.topics.keys())
+            #z = z + 1
+            #if z % np.floor(len(self.bagfiles)/10) == 0:
+            #    print("%0.1f percent complete..." % z/len(self.bagfiles)*100)
     
     def print_topic_sample(self,regexp=None):
         '''A method to print only topics from first 10 files.
         
         This might be useful when the file archive is long, because reading
         get_type_and_topic_info() is slow over a network.'''
-        if self.file_list is None:
+        if len(self.bagfiles) == 0:
             self.find_bag_files(path=self.directory)
         z = 0
-        for bagfile in self.file_list:
-            b = rosbag.Bag(bagfile)
-            print("Processing %s." % bagfile)
+        for baginfo in self.bagfiles:
+            b = rosbag.Bag(baginfo['path'])
+            print("Processing %s." % baginfo["path"])
             tt = b.get_type_and_topic_info()
             self.msg_types = self.msg_types.union(tt.msg_types.keys())
             self.topics = self.topics.union(tt.topics.keys())
@@ -74,7 +89,7 @@ class bag_collection():
             if z > 1:
                 break
 
-        self.print_topics()
+        self.print_topics(regexp=regexp)
 
     def print_topics(self,regexp=None):
         '''Print topics in the collection.
@@ -120,19 +135,34 @@ class bag_collection():
         '''A method to get all occurances of a field in the collection'''
         timestamp = []
         values = []
-        if self.file_list is None:
+        if len(self.bagfiles) == 0:
             self.find_bag_files(path=self.directory)
         z = 0
-        for b in self.file_list:
-            t, f = self.get_field_from_bag(filename=b,topic=topic,field=field)
-            timestamp.append(t)
-            values.append(f)
+        for baginfo in self.bagfiles:
+            t, f = self.get_field_from_bag(filename=baginfo['path'],topic=topic,field=field)
+            timestamp.extend(t)
+            values.extend(f)
             z = z + 1
-            if z > 3:
-                break
+            #if z > 3:
+            #    break
 
         return timestamp,values
 
+    def print_message_def(self,topic=None):
+        '''A method to print the message definition for a topic.'''
 
+        if len(self.bagfiles) == 0:
+            self.find_bag_files(self.directory)
+
+        for baginfo in self.bagfiles:
+            b = rosbag.Bag(baginfo['path'])
+            foundTopoic = False
+            for bagtopic, msg, t in b.read_messages():
+                if bagtopic == topic:
+                    print(msg._full_text)
+                    foundTopic = True
+                    break
+            if foundTopic:
+                break
 
 
